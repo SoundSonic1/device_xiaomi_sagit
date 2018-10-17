8d90a2e4b7c9792b78ba65fc83e76f147ee90e46
arian <arian.kulmer@web.de>
2018-10-18 00:35:27 +0200

    sagit: set lcd density thanks @Verevka :)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,10 @@ TARGET_KERNEL_CONFIG := sagit_defconfig
 # Assert
 TARGET_OTA_ASSERT_DEVICE := sagit
 
+# LCD Density
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.sf.lcd_density=428
+
 # Lineage Hardware
 BOARD_HARDWARE_CLASS += \
     $(DEVICE_PATH)/lineagehw
