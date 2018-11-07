xiaomi-msm8998/android_device_xiaomi_sagit

sagit: call XiaomiCustom if exists <3 XiaomiCustom

device.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# call XiaomiCustom
+$(call inherit-produc-if-exists, vendor/XiaomiCustom/XiaomiCustom.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \

SoundSonic1/device_xiaomi_sagit

device.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# call XiaomiCustom
+$(call inherit-product-if-exists, vendor/XiaomiCustom/XiaomiCustom.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \